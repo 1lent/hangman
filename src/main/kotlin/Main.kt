8472f34c16eed7356dc3c5c349e114e808274dbe
1lent <brodiecuff@outlook.com>
2024-08-24 23:08:05 +0800

    hangmanv1.7 words randomised again when wrong

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,7 +6,8 @@ fun main() {
 
 
     val words = arrayOf("apple", "book", "chair", "dog", "elephant", "fish", "grape", "house", "ice", "jacket", "kite", "lamp", "moon", "nest", "orange", "pencil", "queen", "rain", "sun", "tree")
-    val randomwords = words.randomOrNull()
+    var randomWord = words.random()
+
     for (i in 1..2) {
         println("Loading.. ")
     }
@@ -19,18 +20,21 @@ fun main() {
     var hangmancount = 0
 
     while (true) {
-        if (guess == randomwords) {
+        if (guess == randomWord) {
             println("You got it!")
             break
         } else {
+
             println("\nYou got it wrong!")
             hangmancount++
             println("Hangman errors: $hangmancount")
-            println("It was $randomwords")
+            println("It was $randomWord")
             for (i in 1..2) {
                 println(" ")
             }
             println("Enter a word to guess (lowercase): ")
+            randomWord = words.random()
+            var randomWord = words.random()
             val guess = readln()
             continue
         }
