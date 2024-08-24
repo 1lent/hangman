e99ef00ef2e1809231af2ce841a4f5b7bab84756
1lent <brodiecuff@outlook.com>
2024-08-24 19:26:57 +0800

    hangman1.2 fixed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,25 +4,25 @@ import java.util.*
 
 fun main() {
     val reader = Scanner(System.`in`)
-    println("Enter A Word: ")
-    val first = reader.next()
-    val length = first.count()
 
 
-    for (i in 1..50) {
-        println("[!] MAKING SPACE [!]")
+    val words = arrayOf("apple", "book", "chair", "dog", "elephant", "fish", "grape", "house", "ice", "jacket", "kite", "lamp", "moon", "nest", "orange", "pencil", "queen", "rain", "sun", "tree")
+    val randomwords = words.randomOrNull()
+
+    for (i in 1..10) {
+        println("Loading.. ")
     }
 
 
 
-    println("\nThe word has $length letters")
+    //println("\nThe word has $length letters")
     val result: String
 
     println("Enter a word to guess:")
     val guess = reader.next()
     var hangmancount = 0
 
-    if (guess != first) {
+    if (guess != randomwords) {
         println("You got it wrong")
         hangmancount++
         print("Hangman errors: $hangmancount")
