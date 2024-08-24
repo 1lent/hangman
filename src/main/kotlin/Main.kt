01eb214a134c498a9ad62500f0d53741d172b8a8
1lent <brodiecuff@outlook.com>
2024-08-24 20:51:20 +0800

    hangmanv1.5 continueable game now, fixed formatting and loading etc

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,14 +8,14 @@ fun main() {
     val words = arrayOf("apple", "book", "chair", "dog", "elephant", "fish", "grape", "house", "ice", "jacket", "kite", "lamp", "moon", "nest", "orange", "pencil", "queen", "rain", "sun", "tree")
     val randomwords = words.randomOrNull()
 
-    for (i in 1..10) {
+    for (i in 1..2) {
         println("Loading.. ")
     }
 
     //println("\nThe word has $length letters")
     val result: String
 
-    println("Enter a word to guess (lowecase): ")
+    println("Enter a word to guess (lowecaser): ")
     val guess = reader.next()
     var hangmancount = 0
 
@@ -24,12 +24,13 @@ fun main() {
             println("You got it!")
             break
         } else {
-            println("You got it wrong!")
+            println("\nYou got it wrong!")
             hangmancount++
             println("Hangman errors: $hangmancount")
             println("It was $randomwords")
-            for (i in 1..10) {
-                println("Loading new game...")
+            for (i in 1..2) {
+                println(" ")
+
             }
             println("Enter a word to guess (lowecase): ")
             val guess = reader.next()
